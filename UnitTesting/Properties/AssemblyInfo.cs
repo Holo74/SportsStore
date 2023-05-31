fd342a29d95bd56b97ba6d3ba23e1738f9ea340b
Holo74 <njk0074@gmail.com>
2023-05-30 21:14:48 -0700

    Add project files.

---
+++ UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1eae728f-34b1-422a-8096-6dc27228f89a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTesting/Tests/HomeControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace UnitTesting.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Can_Use_Repository()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"}
+            }).AsQueryable<Product>());
+            HomeController controller = new HomeController(mock.Object);
+            // Act
+            IEnumerable<Product>? result =
+                (controller.Index() as ViewResult)?.ViewData.Model
+                     as IEnumerable<Product>;
+            // Assert
+            Product[] prodArray = result?.ToArray()
+                ?? Array.Empty<Product>();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.Equals("P1", prodArray[0].Name);
+            Assert.Equals("P2", prodArray[1].Name);
+        }
+    }
+}
